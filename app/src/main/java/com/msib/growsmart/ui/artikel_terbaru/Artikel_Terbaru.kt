bedb3d95b83b9228a6ea47a77e9df7555389a99b
bangalexx <ras183375@gmail.com>
2023-12-03 15:05:24 +0700

    Menambahakan Halaman Artikel

---
+++ app/src/main/java/com/msib/growsmart/ui/artikel_terbaru/Artikel_Terbaru.kt
@@ -0,0 +1,12 @@
+package com.msib.growsmart.ui.artikel_terbaru
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.msib.growsmart.R
+
+class Artikel_Terbaru : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artikel_terbaru)
+    }
+}
