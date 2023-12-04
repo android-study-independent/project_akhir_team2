9389d77a805e190dacebcdb033b595da9b14cd11
vicryfahreza <vfahreza49@gmail.com>
2023-12-05 00:43:52 +0700

    menambahkan halaman posting forum dan gallery

--- app/src/main/java/com/msib/growsmart/ui/forum/posting/PostingActivity.kt
+++ app/src/main/java/com/msib/growsmart/ui/forum/posting/PostingActivity.kt
@@ -2,14 +2,55 @@ package com.msib.growsmart.ui.forum.posting
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.msib.growsmart.R
+import com.msib.growsmart.databinding.ActivityPostingBinding
 
 class PostingActivity : AppCompatActivity() {
+    private var currentImageUri: Uri? = null
+    private lateinit var binding: ActivityPostingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_posting)
+        binding = ActivityPostingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initObserver()
+    }
+
+
+    private fun initObserver() {
+        binding.layoutGambar.setOnClickListener {
+            startGallery()
+        }
+    }
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            showImage()
+            currentImageUri = uri
+            binding.btnPosting.isEnabled = true
+            binding.previewImageView.visibility = View.VISIBLE
+        } else {
+            Log.d("Photo Picker", "No media selected")
+        }
+    }
+
+    private fun showImage() {
+        currentImageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.previewImageView.setImageURI(it)
+        }
     }
 
     companion object {
