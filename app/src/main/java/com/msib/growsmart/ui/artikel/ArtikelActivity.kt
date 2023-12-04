e4836b514859691e8e4ecc1dbb9b305f5c91a91e
bangalexx <ras183375@gmail.com>
2023-12-04 21:17:21 +0700

    Membuat tampilan semua Artikel

--- app/src/main/java/com/msib/growsmart/ui/artikel/ArtikelActivity.kt
+++ app/src/main/java/com/msib/growsmart/ui/artikel/ArtikelActivity.kt
@@ -1,12 +1,69 @@
 package com.msib.growsmart.ui.artikel
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.msib.growsmart.R
+import com.msib.growsmart.ui.artikel.data.ApiService
+import com.msib.growsmart.ui.artikel.data.Article
+import com.msib.growsmart.ui.artikel.data.ArticlesResponse
+import com.msib.growsmart.ui.artikel.data.ArtikelAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ArtikelActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: ArtikelAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_halaman_artikel)
+
+        recyclerView = findViewById(R.id.recyclearView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        getAllArticles()
+
     }
+
+    private fun getAllArticles() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://195.35.32.179:8002/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val apiService = retrofit.create(ApiService::class.java)
+
+        val apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyOCwiaWF0IjoxNzAwNzQ2NzIyfQ.ODFt_XcqGHfUGVi5MozPMpoCi67zZGHdUh1zJtCBxbs"
+
+        val call: Call<ArticlesResponse> = apiService.getAllArticles(apiKey)
+        call.enqueue(object : Callback<ArticlesResponse> {
+            override fun onResponse(call: Call<ArticlesResponse>, response: Response<ArticlesResponse>) {
+                if (response.isSuccessful) {
+                    val articles = response.body()?.articles ?: emptyList()
+                    setupRecyclerView(articles)
+                } else {
+                    println("Respons tidak berhasil: ${response.errorBody()}")
+                }
+            }
+
+            override fun onFailure(call: Call<ArticlesResponse>, t: Throwable) {
+                println("Kesalahan jaringan atau permintaan gagal: ${t.message}")
+            }
+        })
+    }
+
+
+
+
+    private fun setupRecyclerView(articles: List<Article>) {
+        adapter = ArtikelAdapter(articles)
+        recyclerView.adapter = adapter
+    }
+
 }
