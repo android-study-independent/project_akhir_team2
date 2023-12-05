311491f7361e6a202bd3b5cec0c56b87256854f2
bangalexx <ras183375@gmail.com>
2023-12-05 17:09:44 +0700

    Membuat tampilan Artikel Terbaru

---
+++ app/src/main/java/com/msib/growsmart/ui/artikel_terbaru/Halaman_Article_Terbaru.kt
@@ -0,0 +1,62 @@
+package com.msib.growsmart.ui.artikel_terbaru
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.msib.growsmart.R
+import com.msib.growsmart.ui.artikel_terbaru.data.Article
+import com.msib.growsmart.ui.artikel_terbaru.data.ArticleAdapter
+import okhttp3.*
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.IOException
+
+class Halaman_Article_Terbaru : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: ArticleAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman_article_terbaru)
+
+        recyclerView = findViewById(R.id.recyclearView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = ArticleAdapter()
+        recyclerView.adapter = adapter
+
+        val id = 1 // Ganti dengan ID yang diinginkan
+        val url = "http://195.35.32.179:8002/article/article_id/$id"
+        val apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyOCwiaWF0IjoxNzAwNzQ2NzIyfQ.ODFt_XcqGHfUGVi5MozPMpoCi67zZGHdUh1zJtCBxbs"
+
+        val request = Request.Builder()
+            .url(url)
+            .addHeader("x-api-key", apiKey)
+            .build()
+
+        val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // Handle failure
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val responseData = response.body?.string()
+
+                runOnUiThread {
+                    if (responseData != null) {
+                        val jsonObject = JSONObject(responseData)
+                        val title = jsonObject.optString("title")
+                        val image = jsonObject.optString("image")
+                        val description = jsonObject.optString("description")
+
+                        // Add the retrieved data to the adapter
+                        adapter.addItem(Article(title, image, description))
+                    }
+                }
+            }
+        })
+    }
+}
---
+++ app/src/main/java/com/msib/growsmart/ui/artikel_terbaru/data/ArticleAdapter.kt
@@ -0,0 +1,48 @@
+package com.msib.growsmart.ui.artikel_terbaru.data
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.msib.growsmart.R
+import com.squareup.picasso.Picasso
+
+class ArticleAdapter : RecyclerView.Adapter<ArticleAdapter.ArticleViewHolder>() {
+
+    private val articles: MutableList<Article> = mutableListOf()
+
+    fun addItem(article: Article) {
+        articles.add(article)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_article, parent, false)
+        return ArticleViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val article = articles[position]
+        holder.bind(article)
+    }
+
+    override fun getItemCount(): Int {
+        return articles.size
+    }
+
+    class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imageView: ImageView = itemView.findViewById(R.id.gbr1)
+        private val titleTextView: TextView = itemView.findViewById(R.id.title1)
+        private val descriptionTextView: TextView = itemView.findViewById(R.id.des1)
+
+        fun bind(article: Article) {
+            titleTextView.text = article.title
+            descriptionTextView.text = article.description ?: "No description available"
+            Picasso.get().load(article.image).into(imageView)
+        }
+    }
+}
+
+data class Article(val title: String, val image: String, val description: String?)
