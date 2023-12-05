84de2684a38be9174f4d397e7c39a914a95355c4
TIAS23 <96928599+TIAS23@users.noreply.github.com>
2023-12-05 14:06:47 +0700

    Cara Menggunakan Aplikasi Pada Fitur LMS

---
+++ app/src/main/java/com/msib/growsmart/ui/lms/lmsCaraMenggunakanAplikasiFragment.kt
@@ -0,0 +1,12 @@
+package com.msib.growsmart.ui.lms
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.msib.growsmart.R
+
+class LmsCaraMenggunakanAplikasiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lms_cara_menggunakan_aplikasi)
+    }
+}
