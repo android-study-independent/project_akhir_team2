e492ce2713a07963e18f8682a82322557d7648ec
vicryfahreza <vfahreza49@gmail.com>
2023-12-09 11:25:21 +0700

    Menambahkan video melalui library exoplayer

--- app/src/main/java/com/msib/growsmart/ui/lms/LmsMediaTanamFragment.kt
+++ app/src/main/java/com/msib/growsmart/ui/lms/LmsMediaTanamFragment.kt
@@ -1,4 +1,4 @@
-package com.msib.growsmart.ui.forum.posting
+package com.msib.growsmart.ui.lms
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [PostingFragment.newInstance] factory method to
+ * Use the [LmsMediaTanamFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class PostingFragment : Fragment() {
+class LmsMediaTanamFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class PostingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_posting, container, false)
+        return inflater.inflate(R.layout.fragment_macam_media_tanam, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class PostingFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment PostingFragment.
+         * @return A new instance of fragment MacamMediaTanamFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            PostingFragment().apply {
+            LmsMediaTanamFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
---
+++ app/src/main/java/com/msib/growsmart/ui/lms/modul/LmsGuideAplikasiActivity.kt
@@ -0,0 +1,36 @@
+package com.msib.growsmart.ui.lms.modul
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import com.msib.growsmart.databinding.ActivityLmsGuideAplikasiBinding
+
+class LmsGuideAplikasiActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLmsGuideAplikasiBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLmsGuideAplikasiBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val videoItem = MediaItem.fromUri("https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4")
+        val player = ExoPlayer.Builder(this).build().also { exoPlayer ->
+            exoPlayer.setMediaItem(videoItem)
+            exoPlayer.prepare()
+        }
+        binding.playerView.player = player
+
+    }
+
+
+    companion object {
+        @JvmStatic
+        fun start(context: Context) {
+            val starter = Intent(context, LmsGuideAplikasiActivity::class.java)
+            context.startActivity(starter)
+        }
+    }
+
+}
